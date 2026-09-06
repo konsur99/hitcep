e4adcebe2e98fc0640eed013e30aff4a5573a1e0
QuikKONI Dev <dev@quikkoni.com>
2026-09-06 17:05:29 +0700

    fix: use native script injection for FontAwesome to prevent missing icons

--- src/components/FontAwesome.tsx
+++ src/components/FontAwesome.tsx
@@ -1,15 +1,17 @@
-"use client";
-
 export default function FontAwesome() {
   return (
     <>
       <link rel="preload" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" as="style" />
-      <link 
-        rel="stylesheet" 
-        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" 
-        media="print" 
-        onLoad={(e) => { (e.target as any).media = 'all'; }} 
-      />
+      <script dangerouslySetInnerHTML={{
+        __html: `
+          (function() {
+            var link = document.createElement('link');
+            link.rel = 'stylesheet';
+            link.href = 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css';
+            document.head.appendChild(link);
+          })();
+        `
+      }} />
       <noscript>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
       </noscript>
